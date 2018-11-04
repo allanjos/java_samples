f930a47fdd5e6697c14d77b81f9ee761ed2c3b87
Allann Jones <allanjos@gmail.com>
2018-11-04 14:40:55 -0400

    Executable JAR with dependencies included
    Signed-off-by: Allann Jones

---
+++ java-jar-exec/src/main/java/br/com/olivum/java/junit/App.java
@@ -0,0 +1,18 @@
+package br.com.olivum.java.junit;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class);
+
+    public App() {
+        logger.debug("App()");
+    }
+
+    public static void main(String args[]) {
+        BasicConfigurator.configure();
+
+        logger.debug("main()");
+    }
+}
---
+++ java-jar-exec/src/test/java/br/com/olivum/java/junit/AppTest.java
@@ -0,0 +1,16 @@
+package br.com.olivum.java.junit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        System.out.println("TestApp.testApp()");
+
+        assertTrue(true);
+    }
+}
