2bd2ec5e15657f4d1fbdb0adc6b7214501e81820
Allann Jones <allanjos@gmail.com>
2018-11-04 22:44:01 -0400

    Fix eclipse project for tomcat app
    Signed-off-by: Allann Jones

--- java-tomcat-eclipse/WebContent/index.jsp
+++ java-tomcat-eclipse/WebContent/index.jsp
@@ -3,7 +3,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Investments</title>
+<title>JSP file</title>
 </head>
 <body>
     1 2 3 4 5
--- java-tomcat-eclipse/src/ServletTest.java
+++ java-tomcat-eclipse/src/ServletTest.java
@@ -9,34 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletTest
  */
-@WebServlet("/ServletTest")
+@WebServlet(urlPatterns={"/test"})
 public class ServletTest extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletTest() {
-        super();
-        // TODO Auto-generated constructor stub
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
