996816c3b6a6101daade707421b84e664f7c9858
Allann Jones <allanjos@gmail.com>
2020-02-26 15:03:16 -0400

    Upgrade to log4j2
    Signed-off-by: Allann Jones

--- java-jar-exec/src/main/java/br/com/olivum/java/execjar/App.java
+++ java-jar-exec/src/main/java/br/com/olivum/java/execjar/App.java
@@ -1,18 +1,12 @@
 package br.com.olivum.java.execjar;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App {
-    private static final Logger logger = Logger.getLogger(App.class);
-
-    public App() {
-        logger.debug("App()");
-    }
+    private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String args[]) {
-        BasicConfigurator.configure();
-
         logger.debug("main()");
     }
 }
--- java-jar-exec/src/test/java/br/com/olivum/java/execjar/AppTest.java
+++ java-jar-exec/src/test/java/br/com/olivum/java/execjar/AppTest.java
@@ -4,12 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-import org.apache.log4j.Logger;
-
 public class AppTest
 {
-    Logger logger = Logger.getLogger(AppTest.class);
-
     @Test
     public void testApp()
     {
